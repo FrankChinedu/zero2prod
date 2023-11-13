7b04ab00455a8d706d39e1658782b4487b5f9ad0
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-11-13 14:51:33 +0100

    add migration and db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,10 @@
 use std::net::TcpListener;
 
+use reqwest::Client;
+
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
-    println!("address {}", address);
-
-    let client = reqwest::Client::new();
+    let (address, client) = get_address_and_client();
     let response = client
         .get(format!("{}/health_check", &address))
         .send()
@@ -14,6 +13,55 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let (app_address, client) = get_address_and_client();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let (app_address, client) = get_address_and_client();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
+fn get_address_and_client() -> (String, Client) {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    (address, client)
 }
 
 fn spawn_app() -> String {
