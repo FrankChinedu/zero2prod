91f183f93e1e495b2575a38503e750526ec11de6
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-11-15 08:33:04 +0100

    connect database and run successful initial test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
 use std::net::TcpListener;
 
 use reqwest::Client;
+use sqlx::{Connection, PgConnection, PgPool};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
-    let (address, client) = get_address_and_client();
+    let (test_app, client) = get_address_and_client().await;
     let response = client
-        .get(format!("{}/health_check", &address))
+        .get(format!("{}/health_check", &test_app.address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -17,22 +19,34 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let (app_address, client) = get_address_and_client();
+    let (test_app, client) = get_address_and_client().await;
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &test_app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
         .expect("Failed to execute request.");
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let (app_address, client) = get_address_and_client();
+    let (test_app, client) = get_address_and_client().await;
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
@@ -41,7 +55,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(&format!("{}/subscriptions", &app_address))
+            .post(&format!("{}/subscriptions", &test_app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -58,17 +72,30 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     }
 }
 
-fn get_address_and_client() -> (String, Client) {
-    let address = spawn_app();
+async fn get_address_and_client() -> (TestApp, Client) {
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
-    (address, client)
+    (test_app, client)
 }
 
-fn spawn_app() -> String {
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("failed to bind address");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let server = zero2prod::run(listener, connection_pool.clone()).expect("failed to bind address");
 
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    tokio::spawn(server);
+
+    TestApp {
+        address: format!("http://127.0.0.1:{}", port),
+        db_pool: connection_pool,
+    }
 }
