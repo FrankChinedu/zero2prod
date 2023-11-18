3751b84655924240f3e193574afddf18c1da3d06
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-11-18 08:18:38 +0100

    setup up dockerfile

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("msg");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
